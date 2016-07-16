fa238d8921fd30a907544ed1f374a30dbace213e
Kenneth Abad <kennethabad2@gmail.com>
2016-07-16 15:46:06 -0400

    Upload basic working game

--- README.md
+++ README.md
@@ -4,3 +4,15 @@ frontend-nanodegree-arcade-game
 Students should use this [rubric](https://www.udacity.com/course/viewer/#!/c-nd001/l-2696458597/m-2687128535) for self-checking their submission.
 
 For detailed instructions on how to get started, check out this [guide](https://docs.google.com/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true).
+
+This is a basic HTML 5 game similar to Frogger. The player starts on the grass blocks and has to make it
+to the water at the other end. Watch out! Giant bugs are trying to stop you. If they catch you,
+you will start again at the starting position.
+
+In order to play the game:
+
+1) Unzip the file
+
+2) Open index.html file
+
+3) The game should load up. Have fun!
--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, z) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = x;
+    this.y = y;
+    this.speed = z; // the speed of the enemy will be assigned to variable z
+
 };
 
 // Update the enemy's position, required method for game
@@ -14,7 +18,17 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + this.speed * dt;
+    if (this.x > 505) {
+        this.x = 0;
+        this.speed = getRandomValue(200, 400);
+    }
+
 };
+
+function getRandomValue(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -25,12 +39,94 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function(x, y) {
 
+    this.sprite = 'images/char-boy.png';
+    this.x = x;
+    this.y = y;
+    this.speed = 100;
+};
+
+Player.prototype.update = function(dt) {
+
+    /*  
+    The two if statements checks (or sets) the boundary of the game.
+    If the player hits the bounday parameters, they will remain within the game map.
+    
+    */
+    if (this.x < 0 || this.x > 400) { // sets the boundaries for x (left and right movement)
+        if (this.x < 0) {
+            this.x = 0;
+        } else {
+            this.x = 400
+        }
+    }
+    if (this.y < 60 || this.y > 380) { // sets the boundaries for y (up and down movement)
+        if (this.y < 60) { // If the player reaches the water, they will reset
+            this.reset();
+            drawWin();
+            
+        } else if (this.y > 380) {
+            this.y = 380;
+        }
+    }
+
+    if (this.collide()) {
+        this.reset();
+    }
+
+};
+
+//Position the player to initial position
+Player.prototype.reset = function() {
+    this.x = 200;
+    this.y = 300;
+};
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+
+//Handles the player movement by adding/subtracting the x/y value according to key inputs
+Player.prototype.handleInput = function(key) {
+    switch (key) {
+
+        case 'left':
+            this.x -= 100;
+            break;
+
+        case 'right':
+            this.x += 100;
+            break;
+
+        case 'up':
+            this.y -= 80;
+            break;
+        case 'down':
+            this.y += 80;
+            break;
+    }
+};
+
+// Sets the collision boundary for the player. If the bug hits the player, the player is reset to initial position
+Player.prototype.collide = function() {
+    for (var i = 0; i < allEnemies.length; i++) {
+        if (this.x < allEnemies[i].x + 50 && this.x + 50 > allEnemies[i].x && this.y < allEnemies[i].y + 30 && this.y + 30 > allEnemies[i].y) {
+            this.reset();
+            break;
+        }
+    }
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
+var player = new Player(200, 300);
+var enemy1 = new Enemy(-100, 60, 300);
+var enemy2 = new Enemy(-100, 140, 350);
+var enemy3 = new Enemy(-100, 225, 400);
+var allEnemies = [enemy1, enemy2, enemy3];
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
@@ -44,3 +140,8 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+function drawWin() {
+    ctx.font = "48px serif";
+    ctx.fillText("A Winner is You", 100, 650);
+};
